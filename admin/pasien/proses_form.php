71799e130d4d35a332abd725c06ce3b623e7c9aa
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-09-08 18:40:00 +0700

    proses input data

---
+++ admin/pasien/proses_form.php
@@ -0,0 +1,16 @@
+<?php
+#koneksi ke database
+include("../koneksi.php");
+#mengambil value dari setiap input
+$nama = $_POST["nama"];
+$tgl = $_POST["tgl"];
+$jk = $_POST["jk"];
+$alamat = $_POST["alamat"];
+
+
+#menuliskan query tambah data ke tabel
+$qry = mysql_query("INSERT INTO pasien (Nama_pasienKliniK, Tanggal_LahirPasien, 	Jenis_KelaminPasien	, Alamat_Pasien VALUES ('$nama, $tgl, $jk, $alamat')");
+
+#pengalihan halaman jika proses tambah selesai
+
+?>
