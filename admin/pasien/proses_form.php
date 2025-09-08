20159bc1a5880e954fd4144ac1bb92a034211b2d
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-09-08 18:47:57 +0700

    membuat proses_form.php

--- admin/pasien/proses_form.php
+++ admin/pasien/proses_form.php
@@ -1,16 +1,18 @@
-<?php
-#koneksi ke database
+<?php 
+###### PROSES TAMBAH DATA ######
+#1. koneksi ke database
 include("../koneksi.php");
-#mengambil value dari setiap input
+
+#2. mengambil value dari setiap input
 $nama = $_POST["nama"];
-$tgl = $_POST["tgl"];
+$tgl_lahir = $_POST["tgl"];
 $jk = $_POST["jk"];
 $alamat = $_POST["alamat"];
 
+#3. menuliskan query tambah data ke tabel
+$qry = mysqli_query($koneksi,"INSERT INTO pasien (Nama_pasienKlinik,Tanggal_LahirPasien,Jenis_KelaminPasien,Alamat_Pasien)
+VALUES('$nama','$tgl_lahir','$jk','$alamat')");
 
-#menuliskan query tambah data ke tabel
-$qry = mysql_query("INSERT INTO pasien (Nama_pasienKliniK, Tanggal_LahirPasien, 	Jenis_KelaminPasien	, Alamat_Pasien VALUES ('$nama, $tgl, $jk, $alamat')");
-
-#pengalihan halaman jika proses tambah selesai
+#5. pengalihan halaman jika proses tambah selesai
 header("location:index.php");
 ?>
