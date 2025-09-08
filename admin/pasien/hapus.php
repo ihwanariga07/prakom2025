28a5fedc572c610ca952cd8af36a7f8846ddd266
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-09-08 20:33:43 +0700

    proses hapus selesai

---
+++ admin/pasien/hapus.php
@@ -0,0 +1,14 @@
+<?php
+
+#membuat koneksi
+include("../koneksi.php");
+
+#mengambil value id hapus
+$id = $_GET["id"];
+
+#menjalankan query hapus 
+$qry = mysqli_query($koneksi,"DELETE FROM pasien WHERE pasienKliniK_ID = '$id'");
+
+#mengalihkan halaman jika sudah terhapus
+header("location:index.php");
+?>
